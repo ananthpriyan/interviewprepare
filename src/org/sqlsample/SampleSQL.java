6e23fc0b20d891a5ea5351ead3d6e10392b41bd4
ananthpriyan <125973589+ananthpriyan@users.noreply.github.com>
2023-06-04 11:01:57 +0530

    First commit

---
+++ src/org/sqlsample/SampleSQL.java
@@ -0,0 +1,47 @@
+package org.sqlsample;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class SampleSQL {
+	
+	public static void main(String[] args) throws ClassNotFoundException, SQLException {
+		
+		//1.Local the driver
+		
+		Class.forName("oracle.jdbc.driver.OracleDriver");
+		
+		//2. Connect to the DataBase
+		
+		Connection connection = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe", "hr", "test1234");
+		
+		//3. Write the sql  query
+		
+		String s = "select * from employees";
+		
+		//4. Prepare the statement
+		
+		PreparedStatement statement = connection.prepareStatement(s);
+		
+		//5.Execute the query
+		
+		ResultSet rs = statement.executeQuery();
+		
+		//6. Iterate the result
+		
+		while (rs.next()) {
+			
+			String string = rs.getString("First_name");
+			String string2 = rs.getString("Last_name");
+			
+			System.out.println(string+string2);
+		}
+		
+		connection.close();
+		
+	}
+	
+}
